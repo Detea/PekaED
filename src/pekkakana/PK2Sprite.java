55408c1de32e3786bca7a4053f2ea5fa9c8573ea
Deta <dennis.ka@web.de>
2017-10-23 23:55:09 +0200

    Added support for sprites version 1.2/1.1

--- src/pekkakana/PK2Sprite.java
+++ src/pekkakana/PK2Sprite.java
@@ -12,9 +12,18 @@ import javax.imageio.ImageIO;
 import data.Settings;
 
 public class PK2Sprite {
+	/*
+	 * THIS IS NOT A FULL IMPLEMENTATION OF THE PK2 SPRITE FILE FORMAT!
+	 * It's only partially implemented, because the editor doesn't need to load the whole file.
+	 */
+	
 	public int type;
 	public char[] imageFile = new char[100];
 	public char[][] soundFiles = new char[7][100];
+	
+	private char[] version13 = {'1', '.', '3', '\0'};
+	private char[] version12 = {'1', '.', '2', '\0'};
+	private char[] version11 = {'1', '.', '1', '\0'};
 	
 	public String filename = "";
 	
@@ -33,7 +42,7 @@ public class PK2Sprite {
 	public int frameHeight;
 	public int frameDistance;
 	
-	public char[] name = new char[30];
+	public char[] name = new char[32];
 	public int width, height;
 	
 	double weight;
@@ -52,10 +61,9 @@ public class PK2Sprite {
 	public BufferedImage image;
 	
 	public PK2Sprite(String file) {
-		loadFile(file);
-		
 		filename = file;
 		
+		loadFile(file);
 		loadBufferedImage();
 	}
 	
@@ -64,87 +72,76 @@ public class PK2Sprite {
 			DataInputStream dis = new DataInputStream(new FileInputStream(new File(Settings.SPRITE_PATH + file)));
 			
 			char[] version = {'1', '.', '3', '\0'};
-			boolean correctVersion = false;
 			
 			for (int i = 0; i < version.length; i++) {
-				if (version[i] == dis.readByte()) {
-					correctVersion = true;
-				} else {
-					correctVersion = false;
-				}
+				version[i] = (char) dis.readByte();
 			}
 			
-			if (correctVersion) {
-				type = Integer.reverseBytes(dis.readInt());
-				
-				for (int i = 0; i < imageFile.length; i++) {
-					imageFile[i] = (char) dis.readByte();
-				}
-				
-				for (int i = 0; i < soundFiles.length; i++) {
-					char[] amount = new char[100];
-					readAmount(amount, dis);
-					soundFiles[i] = amount;
-				}
-				
+			type = Integer.reverseBytes(dis.readInt());
+			
+			if (version[2] == '2' || version[2] == '1') {
+				imageFile = new char[13];
+				soundFiles = new char[7][13];
+			}
+			
+			for (int i = 0; i < imageFile.length; i++) {
+				imageFile[i] = (char) (dis.readByte() & 0xFF);
+			}
+			
+			for (int i = 0; i < soundFiles.length; i++) {
+				char[] amount = new char[soundFiles[0].length];
+				readAmount(amount, dis);
+				soundFiles[i] = amount;
+			}
+			
 
-				// read data that isn't needed, but is yet present
-				for (int i = 0; i < sounds.length; i++) {
-					sounds[i] = dis.readInt();
+			// read data that isn't needed, but is yet present
+			for (int i = 0; i < sounds.length; i++) {
+				sounds[i] = dis.readInt();
+			}
+			
+			frames = (int) (dis.readByte() & 0xFF);
+			
+			/*
+			 	int[] sequence = new int[ANIMATION_MAX_SEQUENCES];
+				int frames; // amount of frames
+				boolean loop; // wether the animations loops, or not
+			 */
+			
+			for (int i = 0; i < animation.length; i++) {
+				int[] sequence = new int[10];
+				int frames = 0;
+				boolean loop = false;
+				
+				for (int j = 0; j < sequence.length; j++) {
+					sequence[j] = (int) (dis.readByte() & 0xFF);
 				}
 				
 				frames = (int) (dis.readByte() & 0xFF);
+				loop = dis.readBoolean();
 				
-				/*
-				 	int[] sequence = new int[ANIMATION_MAX_SEQUENCES];
-					int frames; // amount of frames
-					boolean loop; // wether the animations loops, or not
-				 */
-				
-				for (int i = 0; i < animation.length; i++) {
-					int[] sequence = new int[10];
-					int frames = 0;
-					boolean loop = false;
-					
-					for (int j = 0; j < sequence.length; j++) {
-						sequence[j] = (int) (dis.readByte() & 0xFF);
-					}
-					
-					frames = (int) (dis.readByte() & 0xFF);
-					loop = dis.readBoolean();
-					
-					animation[i] = new PK2SpriteAnimation(sequence, frames, loop);
-				}
-				
-				animations = (int) (dis.readByte() & 0xFF);
-				frameRate = (int) (dis.readByte() & 0xFF);
-				
-				dis.readByte(); //Padding? not documented, though
-				
-				frameX = Integer.reverseBytes(dis.readInt());
-				frameY = Integer.reverseBytes(dis.readInt());
-				
-				frameWidth = Integer.reverseBytes(dis.readInt());
-				frameHeight = Integer.reverseBytes(dis.readInt());
-				frameDistance = Integer.reverseBytes(dis.readInt());
-				
-				// read the sprite's name
-				readAmount(name, dis);
-				
-				// undocumented padding, again?!
-				dis.readShort();
-				
-				width = Integer.reverseBytes(dis.readInt());
-				height = Integer.reverseBytes(dis.readInt());
-			} else {
-				System.out.println("Sprite file wrong version");
+				animation[i] = new PK2SpriteAnimation(sequence, frames, loop);
 			}
+			
+			animations = (int) (dis.readByte() & 0xFF);
+			frameRate = (int) (dis.readByte() & 0xFF);
+			
+			dis.readByte(); //Padding? not documented, though
+			
+			frameX = Integer.reverseBytes(dis.readInt());
+			frameY = Integer.reverseBytes(dis.readInt());
+			
+			frameWidth = Integer.reverseBytes(dis.readInt());
+			frameHeight = Integer.reverseBytes(dis.readInt());
+			frameDistance = Integer.reverseBytes(dis.readInt());
+			
+			// read the sprite's name
+			readAmount(name, dis);
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//System.out.println("Can't find file: " + file);
+			//e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 	}
 	
@@ -162,8 +159,12 @@ public class PK2Sprite {
 		try {
 			BufferedImage tilesheet = ImageIO.read(new File(Settings.SPRITE_PATH + cleanString(imageFile)));
 			
-			image = tilesheet.getSubimage(frameX, frameY, frameWidth, frameHeight);
-			
+			try {
+				image = tilesheet.getSubimage(frameX, frameY, frameWidth, frameHeight);
+			} catch (Exception ex) {
+				System.out.println(filename + " - " + frameX + ", " + frameY + " - " + frameWidth + " - " + frameHeight);
+			}
+				
 			BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
 			 
 		    // make color transparent
@@ -181,8 +182,7 @@ public class PK2Sprite {
 		    
 		    image = result;
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 	}
 	
