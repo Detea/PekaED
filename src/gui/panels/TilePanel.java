d490e34253bf7e4e7a88530e6fad2031421620ed
Detea <dennis.ka@web.de>
2018-03-25 16:32:53 +0200

    Removed unused import

--- src/gui/panels/TilePanel.java
+++ src/gui/panels/TilePanel.java
@@ -6,7 +6,6 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
-import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 
