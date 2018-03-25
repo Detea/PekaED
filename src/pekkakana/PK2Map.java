9f25de57c514e6937d74e591f1a7c948c71211a6
Detea <dennis.ka@web.de>
2018-03-25 16:46:56 +0200

    Removed unused import

--- src/pekkakana/PK2Map.java
+++ src/pekkakana/PK2Map.java
@@ -7,7 +7,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
